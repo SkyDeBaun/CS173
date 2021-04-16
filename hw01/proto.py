60d9218cfba93cd71b8717b263b25f3de9e1d3aa
SkyDeBaun <sdeba001@ucr.edu>
2021-04-16 14:11:28 -0700

    added roman numeral filter

--- hw01/proto.py
+++ hw01/proto.py
@@ -5,18 +5,57 @@ import re
 import nltk
 from nltk.corpus import words #basis for our spell check dictionary
 from nltk.corpus import brown #ammend the dictionary with more words
+from nltk.corpus import gutenberg #ammend the dictionary with more words
+from nltk.corpus import gazetteers
+from nltk.corpus import wordnet
+
+
 import time
+
+
+missing_words = ["edinburgh", "moby", "ahab", "ramadan", "quarterdeck", "whales", "grammars", "lexicons", 
+"psalms", "sylphs", "leviathans", "gills", "swims", "spouts", "trans", "asiatics", "comstock", "breaches","rainbows", "mightier", "spermacetti",
+"chace", "cuvier", "cowper", "suspends", "herberts", "fuzzing", "elbe", "annus", "strafford", "spitzbergen", "kinross", "schouten", "shrouds",
+"nightwatch", "loitering", "reveries", "harpooned", "mastheads", "lookouts", "entrances", "hussey", "etchings", "distended", "rears", "prairies", 
+"quietest", "gazers", "pierheads", "shadiest", "woodlands", "toils", "tribulations", "tigerlilies", "bulwarks", "rebounds", "whalemen", "bakehouses",
+"herrings", "queequeg",]
 
 ##FUNCTIONS--------------------------------------------------------------------------
 #------------------------------------------------------------------------------------
 
 #create set of valid words------------------------------------------- SET for fast lookups -->> idea.. use multiple nltk corpora for dictionary!!!!!
 def create_dictionary_set(dictionary):
+    print("CREATING DICTIONARY-----------------")
+    print("------------------------------------")
+
     #basis--------------------------------------
+    print("Processing NLTK words...")
     for word in words.words():    
         dictionary.add(word.lower())    
     #brown--------------------------------------
-    #add brown and others to dictionary    
+    print("Processing NLTK brown...")
+    for word in brown.words():   
+        #word = re.sub(r'[^a-zA-Z]', '', word)
+        dictionary.add(word.lower()) 
+    #gutenberg-----------------------------------
+    print("Processing NLTK gutenberg...")
+    for word in brown.words():
+        #word = re.sub(r'[^a-zA-Z]', '', word)
+        dictionary.add(word.lower()) 
+    #places--------------------------------------
+    print("Processing NLTK gazetteer(locations)...")
+    for word in gazetteers.words():
+        dictionary.add(word.lower())
+
+    print("Ammending dictionary with curated word list...")
+    for word in missing_words:
+        #print(word)
+        dictionary.add(word)
+        #time.sleep(.15)
+
+
+    print("\nDictionary creation complete!\n")
+    time.sleep(1)
 
 
 #create list of words to validate (ie spelling check)---------------- LIST for sequential access
@@ -26,8 +65,8 @@ def create_word_list(filename, wordlist):
         words = nltk.word_tokenize(text)
     
         for word in words:
-            word = re.sub(r'[^a-zA-Z]', '', word)
-            if word != '': #these aren't getting striped out
+            word = re.sub(r'[^a-zA-Z- ]', '', word)
+            if not re.search(r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$",word, re.IGNORECASE) and word != '': #skip roman numerals                
                 wordlist.append(word.lower().strip())
 
 
@@ -40,7 +79,9 @@ def spell_check(dictionary, wordlist):
             badwords.append(word)
 
     if len(badwords) > 0:
-        print("Bad Words: ")
+        print("Bad Words Found --------------------")
+        print("------------------------------------")
+
         for bad in badwords:
             print(bad)
             time.sleep(.25)
