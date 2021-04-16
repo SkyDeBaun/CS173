001a557ca1564363ed4cf449af117af0d58b87a6
SkyDeBaun <sdeba001@ucr.edu>
2021-04-15 19:30:11 -0700

    added word count

--- hw01/practice/count_sentences.py
+++ hw01/practice/count_sentences.py
@@ -11,13 +11,15 @@ text = "I ate fruit the entire day. For breakfast, I had dates. For lunch, I had
 
 filename = sys.argv[1] #get the filename from user input (via terminal)
 
-#print count of sentences from a given file--------------
+#print count of sentences (and words) from a given file--------------
 def count_sentences(filename):
     with open(filename, 'r') as infile:
         text = infile.read()
+        words = nltk.word_tokenize(text)
         sentences = nltk.sent_tokenize(text)
         count = len(sentences)
         print("Sentence count of " + filename + ": " + str(count) + "\n")
+        print("Word count of " + filename + ": "  + str(len(words)) + "\n")
 
 
 count_sentences(filename)
